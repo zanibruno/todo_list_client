51e03bff35d87c646bf57a16d817fa63ea5eeb84
zanibruno <corporate.zanin@gmail.com>
2020-03-26 13:23:37 +0000

    constructing 1 list and rendering single list name

--- src/components/list.js
+++ src/components/list.js
@@ -1,12 +1,12 @@
 class List {
 
-    constructor(list) {
-        this.id = list.id
-        this.name = list.name
-        this.items = list.items ? list.items : []
-    }
-
-    renderListName() {
-        return `<li class="lists-list" data-id=${this.id}>${this.name}</li>`
-    }
+    // constructor(list) {
+    //     this.id = list.id
+    //     this.name = list.name
+    //     this.items = list.items ? list.items : []
+    // }
+    // 
+    // renderListName() {
+    //     return `<li class="lists-list" data-id=${this.id}>${this.name}</li>`
+    // }
 }
